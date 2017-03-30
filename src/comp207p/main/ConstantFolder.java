69b0f162097d61718834f84b25d27dacc51da753
kazuchan237 <zcabkho@ucl.ac.uk>
2017-03-30 14:03:59 +0100

    simple ldc

--- src/comp207p/main/ConstantFolder.java
+++ src/comp207p/main/ConstantFolder.java
@@ -14,6 +14,7 @@ import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.ICONST;
 import org.apache.bcel.generic.InstructionHandle;
 import org.apache.bcel.generic.InstructionList;
+import org.apache.bcel.generic.Instruction;
 import org.apache.bcel.util.InstructionFinder;
 import org.apache.bcel.generic.MethodGen;
 import org.apache.bcel.generic.TargetLostException;
@@ -22,6 +23,7 @@ import org.apache.bcel.classfile.ConstantPool;
 import org.apache.bcel.classfile.ConstantString;
 import org.apache.bcel.classfile.ConstantUtf8;
 import org.apache.bcel.generic.LDC;
+import org.apache.bcel.generic.IADD;
 
 
 
@@ -61,19 +63,96 @@ public class ConstantFolder
 		for (InstructionHandle handle : instList.getInstructionHandles())
 		{
 			System.out.println(handle);
-			if(handle.getInstruction() instanceof LDC)
+			System.out.println("class name"+ handle.getInstruction().getClass());
+			// if(handle.getInstruction() instanceof LDC)
+			// {
+			// 	System.out.println("LDC");
+			// 	// instList.insert(handle, new LDC(5));
+			// 	try{
+			// 		// System.out.println("delte "+handle);
+			// 		instList.delete(handle);
+			// 	}catch(TargetLostException e)
+			// 	{
+			// 		e.printStackTrace();
+			// 	}
+			//
+			// }
+		}
+		instList.setPositions(true);
+
+		// set max stack/local
+		methodGen.setMaxStack();
+		methodGen.setMaxLocals();
+
+		// generate the new method with replaced iconst
+		Method nMethod = methodGen.getMethod();
+		// replace the method in the original class
+		cgen.replaceMethod(method, nMethod);
+
+	}
+
+
+
+	private int getIntValue(InstructionHandle a, ConstantPoolGen cpgen)
+	{
+		Instruction instruction = a.getInstruction();
+		if(instruction instanceof LDC)
+		{
+			LDC l = (LDC) instruction;
+			int val = (int) l.getValue(cpgen);
+			System.out.println("true");
+			return val;
+		}
+		return 1;
+	}
+
+
+	private void testMethod(ClassGen cgen, ConstantPoolGen cpgen, Method method)
+	{
+		Code methodCode = method.getCode();
+		InstructionList instList = new InstructionList(methodCode.getCode());
+		MethodGen methodGen = new MethodGen(method.getAccessFlags(), method.getReturnType(), method.getArgumentTypes(), null, method.getName(), cgen.getClassName(), instList, cpgen);
+		InstructionHandle[] handles =instList.getInstructionHandles();
+		for(int i = 0; i<handles.length; i++)
+		{
+			InstructionHandle handle = handles[i];
+			System.out.println(handle);
+			System.out.println(handle.getInstruction()); 
+			if(handle.getInstruction() instanceof IADD)
 			{
-				System.out.println("LDC");
-				// instList.insert(handle, new LDC(5));
+				System.out.println("IADD");
+				InstructionHandle a = handles[i-1];
+				int aval = getIntValue(a, cpgen);
 				try{
-					instList.delete(handle);
-				}catch(TargetLostException e)
-				{
+					instList.delete(a);
+				}catch(TargetLostException e){
 					e.printStackTrace();
 				}
 
+				InstructionHandle b = handles[i-2];
+				int bval = getIntValue(b, cpgen);
+				// System.out.println(a+"and"+b);
+				try{
+					instList.delete(b);
+				}catch(TargetLostException e){
+					e.printStackTrace();
+				}
+
+				cpgen.addInteger(aval+bval);
+				instList.insert(handles[i],new LDC(cpgen.getSize()-1));
+				System.out.println("a + b"+(aval+bval));
+				try{
+					instList.delete(handle );
+				}catch(TargetLostException e){
+					e.printStackTrace();
+				}
 			}
 		}
+
+		// IntstructionHandle[] insthandles = instList.getInstructionHandles();
+		// for(int i = 0; i < insthandles.getLength(); i++){
+		// 	System.out.println(insthandles[i]);
+		// }
 		instList.setPositions(true);
 
 		// set max stack/local
@@ -98,7 +177,8 @@ public class ConstantFolder
 		Method[] methods = cgen.getMethods();
 		for (Method m : methods)
 		{
-			optimizeMethod(cgen,cpgen,m);
+			// optimizeMethod(cgen,cpgen,m);
+			testMethod(cgen,cpgen,m);
 
 		}
 
