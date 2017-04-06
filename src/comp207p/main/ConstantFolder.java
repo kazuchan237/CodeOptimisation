0818a5025896aae66ef454d13580bbf3fc8b07b2
kazuchan237 <zcabkho@ucl.ac.uk>
2017-04-06 12:20:44 +0100

    set major 50

--- src/comp207p/main/ConstantFolder.java
+++ src/comp207p/main/ConstantFolder.java
@@ -1148,7 +1148,8 @@ public class ConstantFolder
 
 		// we generate a new class with modifications
 		// and store it in a member variable
-
+		cgen.setConstantPool(cpgen);
+		cgen.setMajor(50); 
 		this.optimized = cgen.getJavaClass();
 
 		// Method[] ms = cgen.getMethods();
