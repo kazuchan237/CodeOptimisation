5975694acc175574ca4dca75adb3da4169c96e6c
William Lam <zcabwhy@ucl.ac.uk>
2017-04-06 11:37:40 +0100

    added back deleteTable

--- src/comp207p/main/ConstantFolder.java
+++ src/comp207p/main/ConstantFolder.java
@@ -759,6 +759,12 @@ public class ConstantFolder
 			}
 			else if((instruction instanceof INVOKEVIRTUAL)||(instruction instanceof GETSTATIC)||(instruction instanceof ReturnInstruction)) {
 				// if(handle.getNext() != null){
+					deleteTable.add(replaceInstructionIndex+1,counter - 1);
+					int size = deleteTable.getSize();
+					System.out.println("SPECIAL");
+					System.out.println(size);
+					System.out.println(deleteTable.getStart(size-1));
+					System.out.println(deleteTable.getEnd(size-1));
 					replaceInstructionIndex = counter + 1;
 					System.out.println("INST: "+replaceInstructionIndex);
 				// }
